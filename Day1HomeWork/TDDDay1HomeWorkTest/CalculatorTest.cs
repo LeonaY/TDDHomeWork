8fc6e65c72cd0b5b1c588a95bfb72fd58c7cb7ea
Leona.Yan <Leona.Yan@USER-PC>
2016-10-17 10:04:30 +0800

    修正11筆資料設定方式

--- Day1HomeWork/TDDDay1HomeWorkTest/CalculatorTest.cs
+++ Day1HomeWork/TDDDay1HomeWorkTest/CalculatorTest.cs
@@ -10,36 +10,45 @@ namespace TDDDay1HomeWorkTest
     {
         [TestMethod]
         
-        public void GetSumOfGroupByCol_取得Cost的總和()
+        public void GetSumOfGroupByCol_有11筆資料3筆一組取Cost總和()
         {
             var expected = new int[] { 6, 15, 24, 21 };
 
             Calculator calculator = new Calculator();
-            IEnumerable<ProductModel> sourceList = GetSourctList();
-            List<int> actual = calculator.GetSumOfGroupByCol<ProductModel>(3, "Cost", sourceList);
+            var sourceList = GetSourctList();
+            var actual = calculator.GetSumOfGroupByCol<ProductModel>(3, "Cost", sourceList);
 
             CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void GetSumOfGroupByCol_取得Revenue的總和()
+        public void GetSumOfGroupByCol_有11筆資料4筆一組取Revenue總和()
         {
             var expected = new int[] { 50, 66, 60 };
 
             Calculator calculator = new Calculator();
-            IEnumerable<ProductModel> sourceList = GetSourctList();
-            List<int> actual = calculator.GetSumOfGroupByCol<ProductModel>(4, "Revenue", sourceList);
+            var sourceList = GetSourctList();
+            var actual = calculator.GetSumOfGroupByCol<ProductModel>(4, "Revenue", sourceList);
 
             CollectionAssert.AreEqual(expected, actual);
         }
 
         private List<ProductModel> GetSourctList()
         {
-            List<ProductModel> model = new List<ProductModel>();
-            for (int i = 1; i <= 11; i++)
+            var model = new List<ProductModel>
             {
-                model.Add(new ProductModel { ID = i, Cost = i, Revenue = 10 + i, SellPrice = 20 + i });
-            }
+                new ProductModel { ID = 1, Cost = 1, Revenue = 11, SellPrice = 21},
+                new ProductModel { ID = 2, Cost = 2, Revenue = 12, SellPrice = 22},
+                new ProductModel { ID = 3, Cost = 3, Revenue = 13, SellPrice = 23},
+                new ProductModel { ID = 4, Cost = 4, Revenue = 14, SellPrice = 24},
+                new ProductModel { ID = 5, Cost = 5, Revenue = 15, SellPrice = 25},
+                new ProductModel { ID = 6, Cost = 6, Revenue = 16, SellPrice = 26},
+                new ProductModel { ID = 7, Cost = 7, Revenue = 17, SellPrice = 27},
+                new ProductModel { ID = 8, Cost = 8, Revenue = 18, SellPrice = 28},
+                new ProductModel { ID = 9, Cost = 9, Revenue = 19, SellPrice = 29},
+                new ProductModel { ID = 10, Cost = 10, Revenue = 20, SellPrice = 30},
+                new ProductModel { ID = 11, Cost = 11, Revenue = 21, SellPrice = 31},
+            };
             return model;
         }
     }
