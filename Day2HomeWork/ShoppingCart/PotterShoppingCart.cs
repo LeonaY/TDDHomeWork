d21d1ce6de05f526f6c4c8a01bad47c5b569ce33
Leona.Yan <Leona.Yan@USER-PC>
2016-10-23 17:36:02 +0800

    修改Production Code ，將Scenario: 一二三集各買了一本，價格應為100*3*0.9=270的紅燈轉為綠燈

--- Day2HomeWork/ShoppingCart/PotterShoppingCart.cs
+++ Day2HomeWork/ShoppingCart/PotterShoppingCart.cs
@@ -10,10 +10,16 @@ namespace ShoppingCart
         {
             double discount = 0;
             var result = 0;
+
             if (buyBooks.Select(x => x.BookID).Distinct().Count() == 2)
             {
                 discount = 0.05;
             }
+            else if (buyBooks.Select(x => x.BookID).Distinct().Count() == 3)
+            {
+                discount = 0.1;
+            }
+
             result = Convert.ToInt16(buyBooks.Sum(x => x.BookPrice) * (1 - discount));
             return result;
         }
